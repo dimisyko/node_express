fd62ff9b0882e24768419c3f455eed8b9190256f
dimisyko <dimitri.syko@hotmail.com>
2022-09-26 16:26:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require("express")
 const path = require("path")
 const app = express()
+const morgan = require('morgan')
+var favicon = require('serve-favicon')
 const fs = require('fs');
-
 const fetch = fs.readFileSync(`./data/detail.json`);
 const data = JSON.parse(fetch)
 
 app.set('view engine', 'pug')
 app.set('views', 'pages')
 
-app.use("/img", express.static(path.resolve(__dirname, "./assets", "img")))
-
-app.use("/js", express.static(path.resolve(__dirname, "./assets", "js")))
-app.use("/style", express.static(path.resolve(__dirname, "./assets", "style")))
+app
+  .use(favicon(__dirname + '/assets/favicon/favicon.ico'))
+  .use(express.static(__dirname + '/assets'))
 
 app.get("/", (req, res) => {
     res.render('index', { data })
@@ -24,7 +24,9 @@ app.get("/about", (req, res) => {
 
 app.get("/detail/:id", (req, res) => {
     const dataDetail = data.detailPage.find( data => data.id === req.params.id)
-    res.render('detail', { dataDetail })
+    const projectIndex = data.detailPage.indexOf(dataDetail)
+    const next = data.detailPage[projectIndex + 1] ? data.detailPage[projectIndex + 1] : data.detailPage[0]
+    res.render('detail', { dataDetail, next })
 })
 app.use((req, res) => {
     res.status(404).render('404')
